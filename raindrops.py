1f9503c2ff6e0eb9976e07ed0e36422e36b72223
loruim <yroslav.strekalov@gmail.com>
2023-09-27 18:30:36 +0600

    Firs homework | All tests passed

--- raindrops.py
+++ raindrops.py
@@ -5,4 +5,19 @@ def convert(number):
     'Plong', если число number делится на 7,
     само число number, если оно не делится ни на 3, ни на 5, ни на 7."""
     # Напишите ваш код здесь
-    return str(number) # эту строку можно удалить
+    if number % 3 == 0 and number % 5 == 0 and number % 7 == 0:
+        return "PlingPlangPlong"
+    elif number % 3 == 0 and number % 5 == 0:
+        return "PlingPlang"
+    elif number % 3 == 0 and number % 7 == 0:
+        return "PlingPlong"
+    elif number % 5 == 0 and number % 7 == 0:
+        return "PlangPlong"
+    elif number % 3 == 0:
+        return "Pling"
+    elif number % 5 == 0:
+        return "Plang"
+    elif number % 7 == 0:
+        return "Plong"
+    else:
+        return str(number)
